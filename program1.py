5516c5327ca7373ccc41f980a1525cb4f9adefec
Julakanti Nikhil Raj <nikhilraj.julakanti@gmail.com>
2024-11-11 16:18:15 +0530

    submit

--- program1.py
+++ program1.py
@@ -4,12 +4,38 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        bracket_map={')':'(','}':'{',']':'['}
+        st=[]
+        for c in s :
+            if c in bracket_map:
+                # if not st:
+                #     return False
+                # top_ele=st.pop() if st else "#"
+                # if bracket_map[c]!=top_ele:
+                #     return False
+                if not st or st[-1]!=bracket_map[c]:
+                    return False
+                st.pop()
+            else:
+                st.append(c)
+        return not st
 
-
-
-
-
+sol=Solution()
+print(sol.isValid("()"))
+print(sol.isValid("()[]{}"))
+print(sol.isValid("(]"))
+print(sol.isValid("([{}])"))
+print(sol.isValid("{[()]}"))
+print(sol.isValid("{[(])}"))
+print(sol.isValid("{{[[(())]]}}"))        
+print(sol.isValid(""))             
+print(sol.isValid("((((()))))"))   
+print(sol.isValid("(()"))         
+print(sol.isValid("())"))         
+print(sol.isValid("((()))"))      
+print(sol.isValid("([)]"))        
+print(sol.isValid("{[()]}"))      
+print(sol.isValid("{[(])}"))      
 
 
     
--- program2.py
+++ program2.py
@@ -4,7 +4,31 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_map={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+
+        total=0
+        prev_val=0
+
+        for c in s:
+            current_val=roman_map[c]
+            if current_val>prev_val:
+                total+=current_val -2*prev_val
+            else:
+                total+=current_val
+            prev_val=current_val
+        return total
+sol=Solution()
+print(sol.romanToInt("III"))
+print(sol.romanToInt("LVIII"))
+print(sol.romanToInt("MCMXCIV"))
 
 
 
